5a658c512331710884951e0a48f757e6c32abef8
leonidlisneskiy <leonidlisnevskiy@gmail.com>
2017-10-11 22:20:51 +0300

    updated/add slick

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,30 @@
 import sayHello from './lib/sayHello.js';
+import slick from 'slick-carousel';
 
-sayHello();
+$(document).ready(function() {
+	sayHello();
+
+	$('.js-slider').slick({
+		autoplay: false,
+		draggable: false,
+		dots: false,
+		arrows: false,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				dots: true
+			}
+		}]
+	});
+
+	$('.js-scroll-to').on("click", function(e){
+		e.preventDefault();
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 1000);
+	});
+
+});
