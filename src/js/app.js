9ae5f7eb873746441059e301842a038f79499f8d
leonidlisneskiy <leonidlisnevskiy@gmail.com>
2017-10-12 21:16:07 +0300

    update/add slick

--- src/js/app.js
+++ src/js/app.js
@@ -2,29 +2,41 @@ import sayHello from './lib/sayHello.js';
 import slick from 'slick-carousel';
 
 $(document).ready(function() {
-	sayHello();
+  sayHello();
 
-	$('.js-slider').slick({
-		autoplay: false,
-		draggable: false,
-		dots: false,
-		arrows: false,
-		slidesToShow: 5,
-		slidesToScroll: 1,
-		responsive: [{
-			breakpoint: 768,
-			settings: {
-				dots: true
-			}
-		}]
-	});
+  $(window).on('resize orientationchange', function() {
+    $('.js-slider').slick('resize');
+  });
 
-	$('.js-scroll-to').on("click", function(e){
-		e.preventDefault();
-		var anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top
-		}, 1000);
-	});
+  $('.js-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2500,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 9999,
+        settings: 'unslick'
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          autoplay: true
+        }
+      }]
+  });
+
+  $('.js-scroll-to').on('click', function(e) {
+    e.preventDefault();
+    var anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000);
+  });
 
 });
